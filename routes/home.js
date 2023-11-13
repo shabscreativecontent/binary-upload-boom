95a6e652c26aa4468e8f9c354562674d8ba2da1f
ShabsCreativeContent <ajideshedrack@gmail.com>
2023-11-13 14:27:57 +0100

    binary-upload-boom Update

--- routes/home.js
+++ routes/home.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const homeController = require('../controllers/home')
 
 router.get('/', homeController.getIndex)
+router.get('/signup',  homeController.getSignup)
 
 module.exports = router
