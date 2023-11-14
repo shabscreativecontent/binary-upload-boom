393e667337964d817ab7b4865aa2e456270a24c7
ShabsCreativeContent <ajideshedrack@gmail.com>
2023-11-14 09:19:04 +0100

    update

--- public/js/main.js
+++
@@ -1,9 +0,0 @@
-const password1 = document.querySelector('.form-control.password1').value
-const password2 = document.querySelector('.form-control.password2').value
-
-
-document.querySelector('#submitButton').addEventListener('click', verfiPassword)
-
-function verfiPassword(){
-   console.log(4);
-}
